c00121d09f400ccae5a2a7d3ec76ef398befc8ef
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-12-09 22:21:17 +0300

    Add project files.

---
+++ MongoDbAjaxProject/DAL/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbAjaxProject.DAL.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? ProductID { get; set; }
+        public string? ProductName { get; set; }
+        public int Stock { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        public string CategoryID { get; set; }
+    }
+}
